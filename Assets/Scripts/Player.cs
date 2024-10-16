578992bddeffe47fa7c3309be969dbe01b02775a
progresso85 <martin.heuripeau@gmail.com>
2024-10-16 16:22:57 +0200

    fix Script Player and add animation Player for walking
    fix Script Player and add animation Player for walking

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float Speed = 5f;
+    Vector2 mouvement;
+    public Animator animator;
+
+    void Update()
+    {
+
+        mouvement.x = Input.GetAxisRaw("Horizontal");
+        mouvement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", mouvement.x);
+        animator.SetFloat("Vertical", mouvement.y);
+        animator.SetFloat("Speed", mouvement.magnitude);
+
+        rb.MovePosition(rb.position + mouvement * Speed * Time.deltaTime);
+
+    }
+}
