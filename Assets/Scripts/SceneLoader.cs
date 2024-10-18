fb313b9e81bfc1501d075e0395a3ccf1dc83b4bf
Killian.V <90335033+KillianVendewinkele@users.noreply.github.com>
2024-10-18 19:22:21 +0200

    :fix: merge with develop (merge conflict broke everything)

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
