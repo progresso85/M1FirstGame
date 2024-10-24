3b6d560610d127f2ab26b0c58068f92336703149
progresso85 <martin.heuripeau@gmail.com>
2024-10-24 10:50:04 +0200

    add sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Références aux fichiers audio (.wav)
-    public AudioClip cityAmbiantSound; 
+    public AudioClip cityAmbiantSound;
     public AudioClip jumpSound;
     public AudioClip trapSound;
     public AudioClip deathSound;
@@ -16,97 +15,87 @@ public class AudioManager : MonoBehaviour
     public AudioClip victorySound;
     public AudioClip walkSound;
 
-
-    
     private AudioSource audioSource;
 
     void Start()
     {
-        
         audioSource = GetComponent<AudioSource>();
 
-        
+        // Appeler PlayBackgroundMusic() pour lancer la musique d'ambiance
         PlayBackgroundMusic();
     }
 
-    
+    public void PlayBackgroundMusic()
+    {
+        // Exemple : jouer la musique d'ambiance de la ville au démarrage
+        PlayCityAmbiant();
+    }
+
     public void PlayCityAmbiant()
     {
         audioSource.clip = cityAmbiantSound;
-        audioSource.loop = true;  
+        audioSource.loop = true;
         audioSource.Play();
     }
 
-    
     public void PlayJumpSound()
     {
-        audioSource.PlayOneShot(jumpSound);  
+        audioSource.PlayOneShot(jumpSound);
     }
 
-    
     public void PlayTrapSound()
     {
-        audioSource.PlayOneShot(trapSound);  
+        audioSource.PlayOneShot(trapSound);
     }
 
-    
     public void PlayDeathSound()
     {
-        audioSource.PlayOneShot(deathSound);  
+        audioSource.PlayOneShot(deathSound);
     }
 
-    
     public void PlayDogSound()
     {
-        audioSource.PlayOneShot(dogSound);  
+        audioSource.PlayOneShot(dogSound);
     }
 
-    
     public void PlayEatSound()
     {
-        audioSource.PlayOneShot(eatSound);  
+        audioSource.PlayOneShot(eatSound);
     }
 
-    
     public void PlayExplosionSound()
     {
-        audioSource.PlayOneShot(explosionSound);  
+        audioSource.PlayOneShot(explosionSound);
     }
 
-     
     public void PlayHitSound()
     {
-        audioSource.PlayOneShot(hitSound);  
+        audioSource.PlayOneShot(hitSound);
     }
 
-    
     public void PlayMenuSound()
     {
         audioSource.clip = menuSound;
-        audioSource.loop = true;  
+        audioSource.loop = true;
         audioSource.Play();
     }
 
-    
     public void PlayTimerStressSound()
     {
         audioSource.clip = timerStressSound;
-        audioSource.loop = true;  
+        audioSource.loop = true;
         audioSource.Play();
     }
 
-    
     public void PlayVictorySound()
     {
-        audioSource.PlayOneShot(victorySound);  
+        audioSource.PlayOneShot(victorySound);
     }
 
-     
     public void PlayWalkSound()
     {
         audioSource.clip = walkSound;
-        audioSource.loop = true;  
+        audioSource.loop = true;
         audioSource.Play();
     }
-
 }
