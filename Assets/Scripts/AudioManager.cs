a2d1ed7f653ecc9f2c6fa2f00ff3c3a44239dd4e
progresso85 <martin.heuripeau@gmail.com>
2024-10-28 11:55:51 +0100

    fix audio walk

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Références aux fichiers audio (.wav)
     public AudioClip cityAmbiantSound;
     public AudioClip jumpSound;
     public AudioClip trapSound;
@@ -21,17 +20,11 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        // Ajouter un AudioSource au GameObject si non présent
         audioSource = gameObject.AddComponent<AudioSource>();
-
-        // Ajouter un autre AudioSource pour les sons de marche
         walkAudioSource = gameObject.AddComponent<AudioSource>();
 
-        // Exemple de musique d'ambiance à démarrer
         PlayCityAmbiant();
     }
-
-    // Méthodes pour jouer les différents sons
 
     public void PlayCityAmbiant()
     {
@@ -40,68 +33,34 @@ public class AudioManager : MonoBehaviour
         audioSource.Play();
     }
 
-    public void PlayJumpSound()
-    {
-        audioSource.PlayOneShot(jumpSound);
-    }
-
-    public void PlayTrapSound()
-    {
-        audioSource.PlayOneShot(trapSound);
-    }
-
-    public void PlayDeathSound()
-    {
-        audioSource.PlayOneShot(deathSound);
-    }
-
-    public void PlayDogSound()
-    {
-        audioSource.PlayOneShot(dogSound);
-    }
-
-    public void PlayEatSound()
-    {
-        audioSource.PlayOneShot(eatSound);
-    }
-
-    public void PlayExplosionSound()
-    {
-        audioSource.PlayOneShot(explosionSound);
-    }
-
-    public void PlayHitSound()
-    {
-        audioSource.PlayOneShot(hitSound);
-    }
-
+    public void PlayJumpSound() => audioSource.PlayOneShot(jumpSound);
+    public void PlayTrapSound() => audioSource.PlayOneShot(trapSound);
+    public void PlayDeathSound() => audioSource.PlayOneShot(deathSound);
+    public void PlayDogSound() => audioSource.PlayOneShot(dogSound);
+    public void PlayEatSound() => audioSource.PlayOneShot(eatSound);
+    public void PlayExplosionSound() => audioSource.PlayOneShot(explosionSound);
+    public void PlayHitSound() => audioSource.PlayOneShot(hitSound);
     public void PlayMenuSound()
     {
         audioSource.clip = menuSound;
         audioSource.loop = true;
         audioSource.Play();
     }
-
     public void PlayTimerStressSound()
     {
         audioSource.clip = timerStressSound;
         audioSource.loop = true;
         audioSource.Play();
     }
-
-    public void PlayVictorySound()
-    {
-        audioSource.PlayOneShot(victorySound);
-    }
+    public void PlayVictorySound() => audioSource.PlayOneShot(victorySound);
 
     // Jouer et arrêter le son de marche
-
     public void PlayWalkSound()
     {
         if (!walkAudioSource.isPlaying)
         {
             walkAudioSource.clip = walkSound;
-            walkAudioSource.loop = true;  // Faire boucler le son de marche
+            walkAudioSource.loop = true;
             walkAudioSource.Play();
         }
     }
@@ -110,7 +69,12 @@ public class AudioManager : MonoBehaviour
     {
         if (walkAudioSource.isPlaying)
         {
-            walkAudioSource.Stop(); // Arrêter le son de marche
+            walkAudioSource.Stop();
         }
+    }
+
+    public bool IsWalkSoundPlaying()
+    {
+        return walkAudioSource.isPlaying;
     }
 }
